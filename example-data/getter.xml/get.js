73dc6a39795d1143369540db78b27d89748e43c9
Rogier Slag <rogier.slag@gmail.com>
2020-07-10 12:40:52 +0200

    Slow down Ziggo polling, properly stop fetching on cancel

--- example-data/getter.xml/get.js
+++ example-data/getter.xml/get.js
@@ -18,6 +18,8 @@ const fs = require('fs');
 const SID = process.env.SID;
 let token = process.env.TOKEN;
 
+let isStopped = false;
+
 if (!SID) {
 	console.error(`Provide the initial session ID as the SID environment variable`);
 	process.exit(1);
@@ -28,8 +30,12 @@ if (!token) {
 	process.exit(1);
 }
 
+async function sleep(timeout) {
+	return new Promise(resolve => setTimeout(resolve, timeout));
+}
+
 async function run() {
-	for (let fun = 0; fun < 65535; fun++) {
+	for (let fun = 1; !isStopped && fun < 600; fun++) {
 		const response = await fetch("http://192.168.178.1/xml/getter.xml", {
 			"headers" : {
 				"Accept" : "application/xml, text/xml, */*; q=0.01",
@@ -55,8 +61,6 @@ async function run() {
 			.map(e => e.trim())
 			.filter(e => e.startsWith('sessionToken='))
 			.map(e => e.split('=')[1])[0];
-
-		console.log(`Switching to new token: ${newToken}`);
 		if (newToken) {
 			token = newToken;
 		}
@@ -67,18 +71,29 @@ async function run() {
 		}
 
 		const filename = `fun=${fun}.xml`;
+		const data = await response.text();
+		const hasLength = data.length > 0;
+
+		if (fun === 1) {
+			// We can validate the token by checking some presence here
+			const hasCorrectAccessLevel = data.includes('<AccessLevel>1</AccessLevel>');
+			if (!hasCorrectAccessLevel || !hasLength || !isXml) {
+				console.error('Token was invalid and rejected by the modem');
+				process.exit(2);
+			}
+		}
+
 		if (isXml) {
-			const data = await response.text();
-			if (data.length === 0) {
+			if (!hasLength) {
 				try {
 					fs.unlinkSync(filename);
 				} catch (e) {
 					// File did not exist to start with
 				}
-				console.log(`Fun=${fun} yielded empty data`);
+				console.info(`Fun=${fun} yielded empty data`);
 			} else {
 				fs.writeFileSync(filename, data);
-				console.log(`Fun=${fun} yielded data`);
+				console.info(`Fun=${fun} yielded data`);
 			}
 		} else {
 			try {
@@ -86,12 +101,19 @@ async function run() {
 			} catch (e) {
 				// File did not exist to start with
 			}
-			console.log(`Fun=${fun} did not yield data`);
+			console.info(`Fun=${fun} did not yield data`);
 		}
+
+		// Don't overload the modem
+		await sleep(100);
 	}
-	console.log(`\n\nFor your next request, use the following token: ${token}`);
+	console.info(`\n\nFor your next request, use the following token: ${token}`);
 }
 
 run();
 
-process.on('SIGINT', () => console.log(`\n\nStopped! For the next request use the following token: ${TOKEN}`));
+process.on('SIGINT', () => {
+	// We will stop the loop so the last request can nicely print the token to be used
+	console.warn(`\n\nStopped!`);
+	isStopped = true;
+});
